8dd168224a289fddce3831f2c7efb79ba8583cf4
unknown <rabeyaskdr990@gmail.com>
2022-03-23 00:25:19 +0600

    updated

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,7 +9,7 @@ const VideoDetail = ({video}) => {
     const videoSrc= `https://www.youtube.com/embed/${video.id.videoId}`
     return(
       <React.Fragment>
-          <Paper elevation={6} style={{height: '70%'}}>
+          <Paper elevation={6} style={{height: '40%'}}>
                 <iframe frameBorder="0" height="100%" width="100%" title="video player" src={videoSrc} />
           </Paper>
           <Paper elevation={6} style={{padding: '15px'}}>
